39d1fde0902ee16da30a82c0739e72c69e3117d3
carlosiib <ntic.carlos@gmail.com>
2022-10-28 15:32:57 -0700

    add basic transpile from textarea value

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import * as esbuild from 'esbuild-wasm'
 
 function App() {
   const [input, setInput] = useState('')
-
-
+  const [code, setCode] = useState('')
+  const ref = useRef<any>()
 
   useEffect(() => {
     const startService = async () => {
-      const service = await esbuild.startService({
+      ref.current = await esbuild.startService({
         worker: true,
         wasmURL: "/esbuild.wasm"
       })
-      console.log(service)
+
     }
     startService()
   }, [])
 
-  function onClick() {
+  async function onClick() {
+    if (!ref.current) return
 
+    // Transpile value from text area
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    })
+
+    setCode(result.code)
   }
 
   return (
@@ -27,7 +35,7 @@ function App() {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre></pre>
+      <pre>{code}</pre>
     </div>
   );
 }
