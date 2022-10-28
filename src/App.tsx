451ec69e5387cabae39179fcb219815f1aa2077b
carlosiib <ntic.carlos@gmail.com>
2022-10-28 15:24:56 -0700

    init esbuild wasm binary file in a naive way

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as esbuild from 'esbuild-wasm'
 
 function App() {
   const [input, setInput] = useState('')
+
+
+
+  useEffect(() => {
+    const startService = async () => {
+      const service = await esbuild.startService({
+        worker: true,
+        wasmURL: "/esbuild.wasm"
+      })
+      console.log(service)
+    }
+    startService()
+  }, [])
 
   function onClick() {
 
