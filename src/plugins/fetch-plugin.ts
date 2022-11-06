815573dc4f31546c5d443061bc02468ee187b013
carlosiib <ntic.carlos@gmail.com>
2022-11-06 16:27:57 -0700

    refactor onLoad  for fetching css and js modules

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,51 +10,74 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad(
+        { filter: /.css$/ },
+        async (args: any) => {
+          const cachedResult =
+            await fileCache.getItem<esbuild.OnLoadResult>(
+              args.path
+            );
+
+          if (cachedResult) {
+            return cachedResult;
+          }
+
+          const { data, request } = await axios.get(
+            args.path
+          );
+
+          const escaped = data
+            .replace(/\n/g, "")
+            .replace(/"/g, '\\"')
+            .replace(/'/g, "\\'");
+
+          const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `;
+
+          const result: esbuild.OnLoadResult = {
+            loader: "jsx",
+            contents,
+            resolveDir: new URL("./", request.responseURL)
+              .pathname,
+          };
+
+          await fileCache.setItem(args.path, result);
+
+          return result;
+        }
+      );
+
+      //JS
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
 
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
+        //Cached layer
+        const cachedResult =
+          await fileCache.getItem<esbuild.OnLoadResult>(
+            args.path
+          );
+
+        if (cachedResult) {
+          return cachedResult;
         }
-
-        // Cached layer
-        // const cachedResult =
-        //   await fileCache.getItem<esbuild.OnLoadResult>(
-        //     args.path
-        //   );
-
-        // if (cachedResult) {
-        //   return cachedResult;
-        // }
 
         const { data, request } = await axios.get(
           args.path
         );
 
-        const fileType = args.path.match(/.css$/)
-          ? "css"
-          : "jsx";
-
-        const escaped = data
-          .replace(/\n/g, "")
-          .replace(/"/g, '\\"')
-          .replace(/'/g, "\\'");
-
-        const contents =
-          fileType === "css"
-            ? `
-            const style = document.createElement('style');
-            style.innerText = '${escaped}';
-            document.head.appendChild(style);
-          `
-            : data;
-
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents,
+          contents: data,
           resolveDir: new URL("./", request.responseURL)
             .pathname,
         };
