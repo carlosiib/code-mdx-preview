28c3bae4a43c305cc08a0d73648a42318a8a5592
carlosiib <ntic.carlos@gmail.com>
2022-11-23 20:21:20 -0700

    code editor type definitions

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,11 @@
 import Editor from "@monaco-editor/react"
 
 export const CodeEditor = () => {
-  return <Editor language="javascript" theme="dark" height="400px" />
+  return <Editor
+    language="javascript"
+    theme="dark"
+    height="400px"
+    options={{
+      wordWrap: 'on'
+    }} />
 }
