d67beee0cc4c29eaa52f514b05084a388cc1adeb
carlosiib <ntic.carlos@gmail.com>
2022-11-27 15:00:51 -0700

    add bulmaswatch style library

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,48 +1,60 @@
-import { useRef } from 'react'
-import Editor, { EditorDidMount } from "@monaco-editor/react"
-import prettier from 'prettier'
-import parser from 'prettier/parser-babel'
+import { useRef } from "react";
+import Editor, {
+  EditorDidMount,
+} from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
-  initialValue: string,
-  onChange: (value: string) => void
-
+  initialValue: string;
+  onChange: (value: string) => void;
 }
 
+export const CodeEditor: React.FC<CodeEditorProps> = ({
+  initialValue,
+  onChange,
+}) => {
+  const editorRef = useRef<any>();
 
-export const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-  const editorRef = useRef<any>()
-
-  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
-    editorRef.current = monacoEditor
+  const onEditorDidMount: EditorDidMount = (
+    getValue,
+    monacoEditor
+  ) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
-      onChange(getValue())
-
-    })
-    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
-  }
+      onChange(getValue());
+    });
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
 
   const onFormatClick = () => {
     //get current editor value
-    const unformatted = editorRef.current.getModel().getValue()
-    console.log(unformatted)
+    const unformatted = editorRef.current
+      .getModel()
+      .getValue();
+    console.log(unformatted);
 
     //format that value
     const formatted = prettier.format(unformatted, {
-      parser: 'babel',
+      parser: "babel",
       plugins: [parser],
       useTabs: false,
       semi: false,
       singleQuote: true,
-    })
+    });
 
     //set the formatted value to editor
-    editorRef.current.setValue(formatted)
-  }
+    editorRef.current.setValue(formatted);
+  };
 
   return (
     <div>
-      <button onClick={onFormatClick}>Format</button>
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <Editor
         editorDidMount={onEditorDidMount}
         value={initialValue}
@@ -50,7 +62,7 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }
         theme="dark"
         height="400px"
         options={{
-          wordWrap: 'on',
+          wordWrap: "on",
           minimap: { enabled: false },
           showUnused: false,
           folding: false,
@@ -58,6 +70,8 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }
           fontSize: 16,
           scrollBeyondLastLine: false,
           automaticLayout: true,
-        }} />
-    </div>)
-}
+        }}
+      />
+    </div>
+  );
+};
