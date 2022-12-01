e6c4222a495aa78d41a51745c976a7081a1c33dd
carlosiib <ntic.carlos@gmail.com>
2022-11-30 19:17:12 -0700

    editor resize constrains

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -6,6 +6,8 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return <ResizableBox
+    maxConstraints={[Infinity, window.innerHeight * 0.9]}
+    minConstraints={[Infinity, 50]}
     width={Infinity}
     height={300}
     resizeHandles={['s']}>
