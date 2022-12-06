c61e4b308ab27b68bf4d9e39e56fa80967af5e90
carlosiib <ntic.carlos@gmail.com>
2022-12-05 20:47:01 -0700

    toggle mdx editor and preview <- 183

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,9 +1,37 @@
+import { useState, useEffect, useRef } from "react"
 import MDEditor from "@uiw/react-md-editor"
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false)
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      // element click on IS inside editor
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        console.log("element click on is inside editor")
+        return
+      }
+
+      setEditing(false)
+    }
+    document.addEventListener('click', listener, { capture: true })
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true })
+    }
+  }, [])
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={"# Header"} />
     </div>
   )
