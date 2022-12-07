cc3b1a475e645b53b6878888fd00d111a6d3ada1
carlosiib <ntic.carlos@gmail.com>
2022-12-06 20:26:10 -0700

    adding redux 129 - 195

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,7 @@
+// Action creators types
+export enum ActionType {
+  MOVE_CELL = "move_cell",
+  DELETE_CEL = "delete_cell",
+  INSERT_CELL_BEFORE = "insert_cell_before",
+  UPDATE_CELL = "update_cell",
+}
