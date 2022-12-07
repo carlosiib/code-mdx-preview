7c653b6848b0aaebe119a813ec3ebd37ec1fe337
carlosiib <ntic.carlos@gmail.com>
2022-12-06 20:10:48 -0700

    mdx editor style  <- 191

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -4,6 +4,7 @@ import './text-editor.css'
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false)
+  const [value, setValue] = useState("# Header")
   const ref = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
@@ -25,15 +26,17 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     )
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   )
 }
