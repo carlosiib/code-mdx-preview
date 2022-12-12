8d79c9b6f04f2d81484a3f5d9ab3176e161749e6
carlosiib <ntic.carlos@gmail.com>
2022-12-11 21:04:58 -0700

    move/swap cell direction reducer 208

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import { stat } from "fs";
 import produce from "immer";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
@@ -35,7 +36,23 @@ const reducer = produce(
         );
         return;
       case ActionType.MOVE_CELL:
-        return state;
+        const { direction } = action.payload;
+        const index = state.order.findIndex(
+          (idx) => idx === action.payload.id
+        );
+        const targetIndex =
+          direction === "up" ? index - 1 : index + 1;
+        // validate if swapping elements are in arr range
+        if (
+          targetIndex < 0 ||
+          targetIndex > state.order.length - 1
+        ) {
+          return;
+        }
+        //swapping elements
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+        return;
       case ActionType.INSERT_CELL_BEFORE:
         return state;
       default:
