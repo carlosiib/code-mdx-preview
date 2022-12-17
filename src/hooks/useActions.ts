33a4a8b1233b998788ccc2c7531229d5bd884c7f
carlosiib <ntic.carlos@gmail.com>
2022-12-17 15:34:45 -0700

    change localstate to redux 220

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../state";
 
+// This will update the store based on an action creator
 export const useActions = () => {
   const dispatch = useDispatch();
   return bindActionCreators(actionCreators, dispatch);
