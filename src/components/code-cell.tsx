42f07fdbeb77c61885f9fc5d723c895271ed3d55
carlosiib <ntic.carlos@gmail.com>
2022-12-22 22:32:28 -0700

    progress bundle bar <-251

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/useActions';
 import { useTypeSelector } from '../hooks/use-typed-selector';
+import './code-cell.css'
 interface CodeCellProps {
   cell: Cell
 }
@@ -41,7 +42,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {!bundle || bundle.loading
+          ? <div className="progress-cover">
+            <progress className='progress is-small is-primary' max="100"> Loading</progress>
+          </div>
+          : <Preview code={bundle.code} err={bundle.err} />}
       </div>
     </Resizable>
   );
