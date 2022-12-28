84d42708d35c506a41710b05b20a02403dff7d87
carlosiib <ntic.carlos@gmail.com>
2022-12-27 20:10:27 -0700

    fix Y resize code cell window behaviour 269

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import { useTypeSelector } from "../hooks/use-typed-selector"
 import CellListItem from "./cell-list-item"
 import AddCell from "./add-cell"
+import './cell-list.css'
 
 const CellList: React.FC = () => {
   // Getting store values
@@ -17,10 +18,12 @@ const CellList: React.FC = () => {
       <AddCell previousCellId={cell.id} />
     </Fragment>
   )
-  return <div>
-    <AddCell forceVisible={cells.length === 0} previousCellId={null} />
-    {renderCELLS}
-  </div>
+  return (
+    <div className='cell-list'>
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+      {renderCELLS}
+    </div>
+  )
 }
 
 export default CellList
